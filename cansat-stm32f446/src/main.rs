c3ff16a6ef162a1b06b688297d3c17df179980d4
Bartosz <bartosz.slowik@student.pk.edu.pl>
2022-12-16 03:24:46 +0100

    made simple blink using sysclock

--- cansat-stm32f446/src/main.rs
+++ cansat-stm32f446/src/main.rs
@@ -4,12 +4,19 @@
 
 use cortex_m_rt::entry;
 use panic_halt as _;
-use stm32f4xx_hal as _;
+use stm32f4xx_hal as hal;
 use cansat::rtt_target::{rtt_init_print, rprintln};
+
+mod blink;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     rprintln!("Hello, world!");
-    loop {}
+    
+    loop {
+        blink::blink_loop();
+        //will never exit function above
+    }
+
 }
