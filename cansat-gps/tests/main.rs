eaca3e64692de56cbca817d466075cc531b89fca
Vixu <lonevixu@gmail.com>
2023-01-10 12:23:50 +0100

    Added unfinished tests for cansat-gps

--- cansat-gps/tests/main.rs
+++ cansat-gps/tests/main.rs
@@ -5,19 +5,19 @@ const FIRST_MSG: &[u8] = b"$GPGLL,,,,,,V,N*64\r\n";
 const SECOND_MSG: &[u8] = b"$GPGLL,,,,,080619.00,V,N*4C\r\n";
 
 #[test]
-fn gps_doesnt_return_msg_if_it_wasnt_read_from_serial() {
+fn gps_last_nmea_returns_none_if_no_msg_received() {
     let uart = mock::Serial::new(FIRST_MSG.to_owned());
     let mut gps = Gps::new(uart);
 
     gps.read_uart().unwrap();
     gps.read_uart().unwrap();
 
-    assert_eq!(None, gps.last_nmea());
+    assert!(gps.last_nmea().is_none());
 }
 
 #[ignore]
 #[test]
-fn gps_returns_msg_if_one_was_read_from_serial() {
+fn gps_last_nmea_returns_msg_if_msg_received() {
     let uart = mock::Serial::new(FIRST_MSG.to_owned());
     let _ = Gps::new(uart);
 
@@ -26,7 +26,7 @@ fn gps_returns_msg_if_one_was_read_from_serial() {
 
 #[ignore]
 #[test]
-fn gps_returns_first_msg_after_starting_reading_next_one() {
+fn gps_last_nmea_returns_first_msg_if_second_not_yet_terminated() {
     let uart = mock::Serial::new([FIRST_MSG, SECOND_MSG].concat());
     let _ = Gps::new(uart);
 
@@ -35,7 +35,7 @@ fn gps_returns_first_msg_after_starting_reading_next_one() {
 
 #[ignore]
 #[test]
-fn gps_returns_second_msg_after_reading_both() {
+fn gps_last_nmea_returns_second_read_msg() {
     let uart = mock::Serial::new([FIRST_MSG, SECOND_MSG].concat());
     let _ = Gps::new(uart);
 
