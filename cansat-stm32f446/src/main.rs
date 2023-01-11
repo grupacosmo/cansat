78e381765bd2b34b503e70abcab84ab35fdd8cf4
Bartosz <bartosz.slowik@student.pk.edu.pl>
2023-01-11 21:05:15 +0100

    addes bme

--- cansat-stm32f446/src/main.rs
+++ cansat-stm32f446/src/main.rs
@@ -9,13 +9,19 @@ use panic_probe as _;
 #[rtic::app(device = stm32f4xx_hal::pac, dispatchers = [EXTI0])]
 mod app {
     use cansat_gps::Gps;
+    use bme280::i2c::BME280;
+    use cortex_m::asm::nop;
     use stm32f4xx_hal::{
-        gpio::{Alternate, Output, PA5, PC10, PC11},
+        gpio::{Alternate,OpenDrain, Output, PA5, PC10, PC11,PB6, PB7},
         pac::{self, TIM3},
         prelude::*,
         serial::{self, Event, Serial3},
         timer::{monotonic::MonoTimerUs, DelayUs},
+        i2c::{DutyCycle, I2c, I2c1, Mode},
+        pac::{I2C1},
     };
+    type Scl =  PB6<Alternate<4, OpenDrain>>;
+    type Sda =  PB7<Alternate<4, OpenDrain>>;
 
     type Rx3 = PC10<Alternate<7>>;
     type Tx3 = PC11<Alternate<7>>;
@@ -29,6 +35,7 @@ mod app {
     struct Local {
         delay: DelayUs<TIM3>,
         led: PA5<Output>,
+        bme: BME280<I2c<I2C1, (Scl, Sda)>>,
     }
 
     #[monotonic(binds = TIM2, default = true)]
@@ -42,10 +49,43 @@ mod app {
         let rcc = device.RCC.constrain();
         let clocks = rcc.cfgr.sysclk(84.MHz()).freeze();
         let mono = device.TIM2.monotonic_us(&clocks);
-        let delay = device.TIM3.delay_us(&clocks);
+        let mut delay = device.TIM3.delay_us(&clocks);
 
         let gpioa = device.GPIOA.split();
+        let gpiob = device.GPIOB.split();
         let gpioc = device.GPIOC.split();
+        
+        // I2C config
+        let i2c_scl = gpiob
+            .pb6
+            .into_alternate()
+            .internal_pull_up(false)
+            .set_open_drain();
+        let i2c_sda = gpiob
+            .pb7
+            .into_alternate()
+            .internal_pull_up(false)
+            .set_open_drain();
+        
+        let i2c = I2c1::new(
+            device.I2C1,
+            (i2c_scl, i2c_sda),
+            Mode::Fast {
+                frequency: 400000.Hz(),
+                duty_cycle: DutyCycle::Ratio2to1,
+            },
+            &clocks,
+        );
+         //Initialize the sensor
+        
+         let mut bme = BME280::new_primary(i2c);
+         bme.init(&mut delay)
+             .map_err(|_| {
+                 defmt::println!("Could not initialize bme280, Error");
+                 panic!();
+             })
+             .unwrap();
+
 
         let led = gpioa.pa5.into_push_pull_output();
 
@@ -65,22 +105,18 @@ mod app {
         };
 
         let shared = Shared { gps };
-        let local = Local { delay, led };
+        let local = Local { delay, led ,bme};
         let monotonics = init::Monotonics(mono);
-
+        bme_measure::spawn().unwrap();
         blink::spawn().unwrap();
 
         (shared, local, monotonics)
     }
-
-    #[idle(local = [delay], shared = [gps])]
+    #[idle()]
     fn idle(ctx: idle::Context) -> ! {
         defmt::info!("Started idle task");
-
-        let _delay = ctx.local.delay;
-
         loop {
-            rtic::export::nop()
+            nop();
         }
     }
 
@@ -102,11 +138,28 @@ mod app {
     }
 
     /// Toggles led every second
-    #[task(local = [led])]
+    #[task(priority= 1 , local = [led])]
     fn blink(ctx: blink::Context) {
         let led = ctx.local.led;
         led.toggle();
         defmt::info!("Blink");
         blink::spawn_after(1.secs()).unwrap();
     }
+    #[task(priority=1, local = [delay,bme])]
+    fn bme_measure(ctx: bme_measure::Context) {
+        let bme = ctx.local.bme;
+        let delay = ctx.local.delay;
+        match bme.measure(delay) {
+            Ok(measurements) => {
+                defmt::info!("Relative Humidity = {}%", measurements.humidity);
+                defmt::info!("Temperature = {} deg C", measurements.temperature);
+                defmt::info!("Pressure = {} pascals", measurements.pressure);
+            }
+            Err(_) => {
+                defmt::info!("Could not read bme280 due to error");
+            }
+        }
+        bme_measure::spawn_after(5.secs()).unwrap();
+    }
+    
 }
