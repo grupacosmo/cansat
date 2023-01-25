b20bf8c0d905f0b6a91cd0943168c14e9c0dbc36
Bartosz <bartosz.slowik@student.pk.edu.pl>
2023-01-25 20:02:14 +0100

    sdmmc init now works

--- cansat-stm32f446/src/main.rs
+++ cansat-stm32f446/src/main.rs
@@ -1,4 +1,4 @@
-//! Binary crate targeting stm32f4 family microcontrollers.
+//! Binary crate targeting stm32f4 family microspi_devs.
 #![deny(unsafe_code)]
 #![no_main]
 #![no_std]
@@ -9,7 +9,7 @@ use panic_probe as _;
 #[rtic::app(device = stm32f4xx_hal::pac, dispatchers = [EXTI0])]
 mod app {
 
-    use bme280::i2c::BME280;
+    ///use bme280::i2c::BME280;
     use cansat_gps::Gps;
     use cortex_m::asm::nop;
     use defmt::Debug2Format;
@@ -21,8 +21,8 @@ mod app {
         serial::{self, Event, Serial3},
         timer::{monotonic::MonoTimerUs, DelayUs},
     };
-    type Scl = PB6<Alternate<4, OpenDrain>>;
-    type Sda = PB7<Alternate<4, OpenDrain>>;
+    //type Scl = PB6<Alternate<4, OpenDrain>>;
+    //type Sda = PB7<Alternate<4, OpenDrain>>;
 
     type Rx3 = PC10<Alternate<7>>;
     type Tx3 = PC11<Alternate<7>>;
@@ -36,7 +36,7 @@ mod app {
     struct Local {
         delay: DelayUs<TIM3>,
         led: PA5<Output>,
-        bme: BME280<I2c1<(Scl, Sda)>>,
+       //// bme: BME280<I2c1<(Scl, Sda)>>,
     }
 
     #[monotonic(binds = TIM2, default = true)]
@@ -56,31 +56,7 @@ mod app {
         let gpiob = device.GPIOB.split();
         let gpioc = device.GPIOC.split();
 
-        let i2c_scl = gpiob
-            .pb6
-            .into_alternate()
-            .internal_pull_up(false)
-            .set_open_drain();
-        let i2c_sda = gpiob
-            .pb7
-            .into_alternate()
-            .internal_pull_up(false)
-            .set_open_drain();
-
-        let i2c = I2c1::new(
-            device.I2C1,
-            (i2c_scl, i2c_sda),
-            Mode::Fast {
-                frequency: 400000.Hz(),
-                duty_cycle: DutyCycle::Ratio2to1,
-            },
-            &clocks,
-        );
-
-        let mut bme = BME280::new_primary(i2c);
-        if let Err(e) = bme.init(&mut delay) {
-            defmt::panic!("Failed to initalize bme280: {}", Debug2Format(&e));
-        }
+       
 
         let led = gpioa.pa5.into_push_pull_output();
 
@@ -100,59 +76,70 @@ mod app {
         };
 
         let shared = Shared { gps };
-        let local = Local { delay, led, bme };
+        let local = Local { delay, led };
         let monotonics = init::Monotonics(mono);
         ////////////////SDMMC//////////////////////////////////
         use embedded_sdmmc::{Controller, SdMmcSpi, TimeSource, Timestamp, VolumeIdx};
         use stm32f4xx_hal::spi::{Phase, Polarity, Spi};
-
-        let sdmmc_spi = Spi::new(
-            device.SPI2,
-            (gpiob.pb13, gpiob.pb14, gpiob.pb15),
+        use stm32f4xx_hal::pac::SPI1;
+        use stm32f4xx_hal::gpio::Pin;
+        use cortex_m_rt::entry;
+        use stm32f4xx_hal::{
+            pac::{self},
+            //prelude::*,
+            timer::Channel,
+        };
+       
+        let sdmmc_spi = device.SPI1.spi(
+            (
+                gpiob.pb3.into_alternate(),
+                gpioa.pa6.into_alternate(),
+                gpioa.pa7.into_alternate(),
+            ),
             stm32f4xx_hal::spi::Mode {
                 polarity: Polarity::IdleLow,
                 phase: Phase::CaptureOnFirstTransition,
             },
-            400.kHz(),
+            
+            400000.Hz(),
             &clocks,
         );
+        struct Clock;
 
-        let sdmmc_cs = gpiob.pb12.into_push_pull_output();
-        let mut controller = Controller::new(SdMmcSpi::new(sdmmc_spi, sdmmc_cs), FakeTime {});
-
-        defmt::info!("Init SD card...\r");
-        match controller.device().init() {
-            Ok(_) => {
-                defmt::info!("Card size... ");
-                match controller.device().card_size_bytes() {
-                    Ok(size) => defmt::info!("{}\r", size),
-                    Err(e) => defmt::error!("Err: {:?}", Debug2Format(&e)),
-                }
-                defmt::info!("Volume 0:\r");
-                match controller.get_volume(VolumeIdx(0)) {
-                    Ok(volume) => {
-                        let root_dir = controller.open_root_dir(&volume);
-                        defmt::info!("Listing root directory:\r");
-                        controller
-                            .iterate_dir(&volume, &root_dir, |x| {
-                                defmt::info!("Found: {:?}\r", x.name);
-                            })
-                            .unwrap;
-                        defmt::info!("End of listing\r");
-                    }
-                    Err(e) => defmt::error!("Err: {:?}", Debug2Format(&e)),
-                }
-            }
-            Err(e) => defmt::error!("{:?}!", Debug2Format(&e)),
-        }
-        struct FakeTime;
-        impl TimeSource for FakeTime {
+        impl TimeSource for Clock {
             fn get_timestamp(&self) -> Timestamp {
-                Timestamp::from_calendar(1019, 11, 24, 3, 40, 31).unwrap()
+                Timestamp {
+                    year_since_1970: 0,
+                    zero_indexed_month: 0,
+                    zero_indexed_day: 0,
+                    hours: 0,
+                    minutes: 0,
+                    seconds: 0,
+                }
             }
         }
+        let sdmmc_cs = gpioa.pa15.into_push_pull_output();
+        let mut spi_dev = embedded_sdmmc::SdMmcSpi::new(sdmmc_spi, sdmmc_cs);
+        
+        defmt::info!("Init SD card...");
+        match spi_dev.acquire() {
+            Ok(sdmmc_spi) => {
+                let mut cont = Controller::new(sdmmc_spi, Clock);
+                defmt::info!("OK!\nCard size...");
+                match cont.device().card_size_bytes() {
+                    Ok(size) => defmt::info!("{}", size),
+                    Err(e) => defmt::info!("Err: {:?}", e),
+                }
+                defmt::info!("Volume 0...");
+                match cont.get_volume(embedded_sdmmc::VolumeIdx(0)) {
+                    Ok(v) => defmt::info!("{}",v),
+                    Err(e) => defmt::info!("Err: {:?}", e),
+                }
+            }
+            Err(e) => defmt::info!("{:?}!", e)
+        };
         /////////END-OF SDMMC////////////////////////////////////
-        bme_measure::spawn().unwrap();
+       
         blink::spawn().unwrap();
 
         (shared, local, monotonics)
@@ -182,6 +169,7 @@ mod app {
             log_nmea::spawn().unwrap();
         }
     }
+   
 
     /// Toggles led every second
     #[task(local = [led])]
@@ -190,25 +178,5 @@ mod app {
         led.toggle();
         defmt::debug!("Blink");
         blink::spawn_after(1.secs()).unwrap();
-    }
-
-    #[task(local = [delay, bme])]
-    fn bme_measure(ctx: bme_measure::Context) {
-        let bme = ctx.local.bme;
-        let delay = ctx.local.delay;
-        let measurements = match bme.measure(delay) {
-            Ok(m) => m,
-            Err(e) => {
-                defmt::error!("Could not read bme280 measurements: {}", Debug2Format(&e));
-                return;
-            }
-        };
-        let altitude = cansat::calculate_altitude(measurements.pressure);
-        defmt::info!("Altitude = {} meters above sea level", altitude);
-        defmt::info!("Relative Humidity = {}%", measurements.humidity);
-        defmt::info!("Temperature = {} deg C", measurements.temperature);
-        defmt::info!("Pressure = {} pascals", measurements.pressure);
-
-        bme_measure::spawn_after(5.secs()).unwrap();
     }
 }
