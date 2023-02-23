b21c0c41632cff1a1abb1964c8e9a0214a31202b
wiktorwieclaw <wiktorwieclaw@users.noreply.github.com>
2023-02-23 17:12:15 +0000

    deploy: 46a196cb6c596d0092d84d81c0a1bdb943280a44

--- implementors/embedded_sdmmc/filesystem/timestamp/trait.TimeSource.js
+++ implementors/embedded_sdmmc/filesystem/timestamp/trait.TimeSource.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"cansat_stm32f446":[["impl TimeSource for <a class=\"struct\" href=\"cansat_stm32f446/app/struct.Clock.html\" title=\"struct cansat_stm32f446::app::Clock\">Clock</a>"]]
+"cansat_stm32f446":[["impl TimeSource for <a class=\"struct\" href=\"cansat_stm32f446/struct.DummyClock.html\" title=\"struct cansat_stm32f446::DummyClock\">DummyClock</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
