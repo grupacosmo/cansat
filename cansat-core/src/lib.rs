3363ffdae08ec71fd3d5e89b781264f1448af255
Kinga Madej <kinga.madej01@gmail.com>
2023-02-24 19:16:49 +0100

    Added Pressure type to cansat-core

--- cansat-core/src/lib.rs
+++ cansat-core/src/lib.rs
@@ -3,10 +3,36 @@
 #![no_std]
 
 use libm::powf;
+pub struct Pressure {
+    value: f32,
+}
+
+impl Pressure {
+    pub const fn new(_value: f32) -> Pressure {
+        Pressure {value: (_value) }
+    }
+
+    pub const fn from_pascals(value: f32) -> Pressure {
+        Pressure::new(value)
+    }
+
+    pub fn from_hectos(value: f32) -> Pressure {
+        Pressure::new(value * 100.)
+    }
+
+    pub fn as_pascals(&self) -> f32 {
+        self.value
+    }
+
+    pub fn as_hectos(&self) -> f32 {
+        self.value / 100.
+    }
+}
+
 
 // TODO: make it weather dependent
-pub fn calculate_altitude(pressure: f32) -> f32 {
-    let base = pressure / 101300.; //in Pa
+pub fn calculate_altitude(pressure: Pressure) -> f32 {
+    let base = pressure.as_pascals() / 101300.; //in Pa
     let power = 1. / 5.255;
     44330. * (1. - (powf(base, power)))
 }
