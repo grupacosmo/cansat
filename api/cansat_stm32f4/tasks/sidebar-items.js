b7d4c4f26a72ad0c0f48be303d43608cd4d3b3b6
wiktorwieclaw <wiktorwieclaw@users.noreply.github.com>
2023-03-05 15:53:21 +0000

    deploy: f51f79a52f1c65ec11bc9f5be38c33ccc21b5a45

--- api/cansat_stm32f4/tasks/sidebar-items.js
+++ api/cansat_stm32f4/tasks/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["blink","Toggles led every second"],["bme_measure",""],["gps_irq","USART3 interrupt handler that reads data into the gps working buffer"],["log_nmea",""],["sdmmc_log",""]]};
+window.SIDEBAR_ITEMS = {"fn":[["blink","Toggles led every second"],["gps_irq","USART3 interrupt handler that reads data into the gps working buffer"],["idle",""],["sdmmc_log",""]]};
