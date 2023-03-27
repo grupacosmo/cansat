bb55ea1dce73d453d65b863184106572b75097af
Kinga Madej <kinga.madej01@gmail.com>
2023-03-27 13:16:14 +0200

    Removed print!

--- cansat-gps/tests/main.rs
+++ cansat-gps/tests/main.rs
@@ -22,10 +22,7 @@ fn gps_last_nmea_returns_msg_if_msg_received() {
 
     // Read until the first message is fully received (the second element of the tuple returned by read_serial() is true)
     // So as long as it is 'false', it means that the read operation has not yet been completed, and only when the value becomes 'true', it means that the entire GPS message has been read.
-    // to print _read_data use 'cargo test --package cansat-gps -- --nocapture' command
-    while let Ok((_read_data, false)) = gps.read_serial() {
-        //print!("{}", _read_data);
-    }
+    while let Ok((_read_data, false)) = gps.read_serial() {}
 
     // Check if the message has been successfully received (i.e., when in 'is_some()' the returned value is not empty = true).
     assert!(gps.last_nmea().is_some());
@@ -37,9 +34,7 @@ fn gps_last_nmea_returns_first_msg_if_second_not_yet_terminated() {
     let mut gps = Gps::new(uart);
 
     // Similar to the comments above
-    while let Ok((_read_data, false)) = gps.read_serial() {
-        // print!("{}", _read_data);
-    }
+    while let Ok((_read_data, false)) = gps.read_serial() {}
 
     // Check if the first message is available.
     assert_eq!(gps.last_nmea().unwrap(), FIRST_MSG);
@@ -51,12 +46,8 @@ fn gps_last_nmea_returns_second_read_msg() {
     let mut gps = Gps::new(uart);
 
     // Similar to the comments above
-    while let Ok((_read_data, false)) = gps.read_serial() {
-        // print!("{}", _read_data);
-    }
-    while let Ok((_read_data, false)) = gps.read_serial() {
-        // print!("{}", _read_data);
-    }
+    while let Ok((_read_data, false)) = gps.read_serial() {}
+    while let Ok((_read_data, false)) = gps.read_serial() {}
 
     // Check if the second message is available.
     assert_eq!(gps.last_nmea().unwrap(), SECOND_MSG);
