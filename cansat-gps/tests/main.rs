3bd5e1978e2ba6de7238996c87a2e1e3f9fe7de2
Kinga Madej <kinga.madej01@gmail.com>
2023-03-28 21:41:37 +0200

    Updated 3rd gps test

--- cansat-gps/tests/main.rs
+++ cansat-gps/tests/main.rs
@@ -1,5 +1,6 @@
 use cansat_gps::Gps;
 use cansat_test_utils::mock;
+use rand::Rng;
 
 const FIRST_MSG: &[u8] = b"$GPGLL,,,,,,V,N*64\r\n";
 const SECOND_MSG: &[u8] = b"$GPGLL,,,,,080619.00,V,N*4C\r\n";
@@ -29,8 +30,14 @@ fn gps_last_nmea_returns_msg_if_msg_received() {
 fn gps_last_nmea_returns_first_msg_if_second_not_yet_terminated() {
     let uart = mock::Serial::new([FIRST_MSG, SECOND_MSG].concat());
     let mut gps = Gps::new(uart);
+    let mut rng = rand::thread_rng();
 
     while let Ok((_read_data, false)) = gps.read_serial() {}
+
+    for _ in 0..rng.gen_range(1..SECOND_MSG.len()) {
+        gps.read_serial().unwrap();
+    }
+
 
     assert_eq!(gps.last_nmea().unwrap(), FIRST_MSG);
 }
