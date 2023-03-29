0d55b5af3ded8bd5517f2dddf8310d4bc72887f2
Vixu <lonevixu@gmail.com>
2023-03-29 20:40:12 +0200

    added writing data to sd in csv format

--- cansat-stm32f4/src/tasks.rs
+++ cansat-stm32f4/src/tasks.rs
@@ -1,45 +1,117 @@
 use crate::app;
-use accelerometer::RawAccelerometer;
+use accelerometer::{vector, Orientation, RawAccelerometer};
 use cansat_core::quantity::Pressure;
+use cansat_gps::MAX_NMEA_LEN;
+use heapless::Vec;
 use rtic::Mutex;
 use stm32f4xx_hal::prelude::*;
 
-pub fn idle(ctx: app::idle::Context) -> ! {
-    let i2c1_devices = ctx.local.i2c1_devices;
-    let delay = ctx.local.delay;
-    let sd_logger = ctx.local.sd_logger;
-    let mut gps = ctx.shared.gps;
-    let tracker = ctx.local.tracker;
+#[derive(Default)]
+struct RecordData {
+    temperature: Option<f32>,
+    pressure: Option<f32>,
+    altitude: Option<f32>,
+    nmea: Option<Vec<u8, MAX_NMEA_LEN>>,
+    acceleration: Option<vector::I16x3>,
+    orientation: Option<Orientation>,
+}
+
+pub fn idle(mut ctx: app::idle::Context) -> ! {
+    let mut csv_writer = csv_core::WriterBuilder::new().build();
+    let mut buf: Vec<u8, 1000> = Vec::new();
 
     loop {
-        match i2c1_devices.bme280.measure(delay) {
-            Ok(m) => {
-                let altitude = cansat_core::calculate_altitude(Pressure::from_pascals(m.pressure));
-                defmt::info!("Altitude = {} meters above sea level", altitude);
-                defmt::info!("Relative Humidity = {}%", m.humidity);
-                defmt::info!("Temperature = {} deg C", m.temperature);
-                defmt::info!("Pressure = {} pascals", m.pressure);
-            }
-            Err(e) => {
-                defmt::error!(
-                    "Could not read bme280 measurements: {}",
-                    defmt::Debug2Format(&e)
-                );
-            }
-        };
+        let data = read_record_data(&mut ctx);
 
-        if let Some(msg) = gps.lock(|gps| gps.last_nmea()) {
-            defmt::info!("GPS: {=[u8]:a}", &msg);
-            let _ = sd_logger.write(&msg);
+        write_option_f32(&mut csv_writer, data.temperature, &mut buf);
+        csv_writer.delimiter(&mut buf);
+
+        write_option_f32(&mut csv_writer, data.pressure, &mut buf);
+        csv_writer.delimiter(&mut buf);
+
+        write_option_f32(&mut csv_writer, data.altitude, &mut buf);
+        csv_writer.delimiter(&mut buf);
+
+        if let Some(nmea) = data.nmea {
+            csv_writer.field(&nmea, &mut buf);
         }
+        csv_writer.delimiter(&mut buf);
 
-        let accel = i2c1_devices.lis3dh.accel_raw().unwrap();
-        let orientation = tracker.update(accel);
-        defmt::info!("Accelerometer vector {:?}", defmt::Debug2Format(&accel));
-        defmt::info!(
-            "Predicted position: {:?}",
-            defmt::Debug2Format(&orientation)
-        );
+        // TODO:
+        // write acceleration and orientation
+
+        csv_writer.terminator(&mut buf);
+        csv_writer.finish(&mut buf);
+
+        let sd_logger = &mut ctx.local.sd_logger;
+        sd_logger.write(&buf).unwrap();
+    }
+}
+
+fn read_record_data(ctx: &mut app::idle::Context) -> RecordData {
+    let i2c1_devices = &mut ctx.local.i2c1_devices;
+    let delay = &mut ctx.local.delay;
+    let gps = &mut ctx.shared.gps;
+    let tracker = &mut ctx.local.tracker;
+
+    let mut data = RecordData::default();
+
+    match i2c1_devices.bme280.measure(delay) {
+        Ok(m) => {
+            let altitude = cansat_core::calculate_altitude(Pressure::from_pascals(m.pressure));
+
+            defmt::info!(
+                "Temperature: {}°C\r\nPressure: {}Pa\n\rAltitude: {}m",
+                m.temperature,
+                m.pressure,
+                altitude
+            );
+
+            data.temperature = Some(m.temperature);
+            data.pressure = Some(m.pressure);
+            data.altitude = Some(altitude);
+        }
+        Err(e) => {
+            defmt::error!(
+                "Could not read bme280 measurements: {}",
+                defmt::Debug2Format(&e)
+            );
+        }
+    };
+
+    if let Some(nmea) = gps.lock(|gps| gps.last_nmea()) {
+        defmt::info!("NMEA: {=[u8]:a}", &nmea);
+        data.nmea = Some(nmea);
+    }
+
+    match i2c1_devices.lis3dh.accel_raw() {
+        Ok(accel) => {
+            let orientation = tracker.update(accel);
+
+            defmt::info!(
+                "Acceleration: {}, {}, {}\r\nOrientation: {}",
+                accel.x,
+                accel.y,
+                accel.z,
+                defmt::Debug2Format(&orientation)
+            );
+
+            data.acceleration = Some(accel);
+            data.orientation = Some(orientation);
+        }
+        Err(e) => {
+            defmt::error!("Could not read acceleration: {}", defmt::Debug2Format(&e));
+        }
+    }
+
+    data
+}
+
+fn write_option_f32(w: &mut csv_core::Writer, f: Option<f32>, out: &mut [u8]) {
+    let mut buf = ryu::Buffer::new();
+    if let Some(f) = f {
+        let f = buf.format(f);
+        w.field(f.as_bytes(), out);
     }
 }
 
