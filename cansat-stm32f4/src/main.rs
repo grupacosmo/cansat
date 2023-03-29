3884db22c1b2e0ac144614766caa7be2ba4c7462
Vixu <lonevixu@gmail.com>
2023-03-29 22:15:26 +0200

    move type aliases to startup and reexport in main

--- cansat-stm32f4/src/main.rs
+++ cansat-stm32f4/src/main.rs
@@ -9,48 +9,11 @@ mod startup;
 mod tasks;
 
 pub use sd_logger::SdLogger;
+pub use startup::{Bme280, Delay, Gps, I2c1Devices, Led, Monotonic, SdmmcController};
 
 use defmt_rtt as _;
 use panic_probe as _;
-use stm32f4xx_hal::{
-    gpio, i2c, pac, serial, spi,
-    timer::{monotonic::MonoTimerUs, DelayUs},
-};
 use tasks::*;
-
-type Monotonic = MonoTimerUs<pac::TIM2>;
-type Delay = DelayUs<pac::TIM3>;
-type Led = gpio::PC13<gpio::Output>;
-type Gps = cansat_gps::Gps<Serial1>;
-type SdmmcController =
-    embedded_sdmmc::Controller<BlockSpi2, DummyClock, MAX_OPEN_DIRS, MAX_OPEN_FILES>;
-type I2c1Proxy = shared_bus::I2cProxy<'static, shared_bus::AtomicCheckMutex<I2c1>>;
-type Bme280 = bme280::i2c::BME280<I2c1Proxy>;
-type Lis3dh = lis3dh::Lis3dh<lis3dh::Lis3dhI2C<I2c1Proxy>>;
-
-type BlockSpi2 = embedded_sdmmc::BlockSpi<'static, Spi2, Cs2>;
-const MAX_OPEN_DIRS: usize = 4;
-const MAX_OPEN_FILES: usize = 4;
-
-type I2c1 = i2c::I2c1<(Scl1, Sda1)>;
-type Scl1 = gpio::PB8<gpio::Alternate<4, gpio::OpenDrain>>;
-type Sda1 = gpio::PB9<gpio::Alternate<4, gpio::OpenDrain>>;
-
-type Serial1 = serial::Serial1<(Tx1, Rx1)>;
-type Tx1 = gpio::PB6<gpio::Alternate<7>>;
-type Rx1 = gpio::PB7<gpio::Alternate<7>>;
-
-type Spi2Device = embedded_sdmmc::SdMmcSpi<Spi2, Cs2>;
-type Spi2 = spi::Spi2<(Sck2, Miso2, Mosi2)>;
-type Cs2 = gpio::PB12<gpio::Output>;
-type Sck2 = gpio::PB13<gpio::Alternate<5>>;
-type Miso2 = gpio::PB14<gpio::Alternate<5>>;
-type Mosi2 = gpio::PB15<gpio::Alternate<5>>;
-
-pub struct I2c1Devices {
-    pub bme280: Bme280,
-    pub lis3dh: Lis3dh,
-}
 
 #[rtic::app(device = stm32f4xx_hal::pac, dispatchers = [EXTI0, EXTI1])]
 mod app {
@@ -63,7 +26,7 @@ mod app {
 
     #[local]
     struct Local {
-        delay: DelayUs<pac::TIM3>,
+        delay: Delay,
         led: Led,
         sd_logger: SdLogger,
         tracker: accelerometer::Tracker,
@@ -71,23 +34,9 @@ mod app {
     }
 
     #[monotonic(binds = TIM2, default = true)]
-    type MicrosecMono = MonoTimerUs<pac::TIM2>;
+    type MicrosecMono = Monotonic;
 
-    extern "Rust" {
-        #[task(binds = USART1, shared = [gps], priority = 2)]
-        fn gps_irq(ctx: gps_irq::Context);
-
-        #[task(local = [led], priority = 1)]
-        fn blink(ctx: blink::Context);
-
-    }
-
-    #[idle(local = [delay, sd_logger, tracker, i2c1_devices], shared = [gps])]
-    fn idle(ctx: idle::Context) -> ! {
-        tasks::idle(ctx)
-    }
-
-    #[init(local = [spi2_device: Option<Spi2Device> = None])]
+    #[init(local = [spi2_device: Option<startup::Spi2Device> = None])]
     fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
         let board = startup::init_board(ctx.device);
         let cansat = startup::init_drivers(board, ctx.local.spi2_device).unwrap_or_else(|e| {
@@ -108,19 +57,18 @@ mod app {
 
         (shared, local, monotonics)
     }
-}
 
-pub struct DummyClock;
+    #[idle(local = [delay, sd_logger, tracker, i2c1_devices], shared = [gps])]
+    fn idle(ctx: idle::Context) -> ! {
+        tasks::idle(ctx)
+    }
 
-impl embedded_sdmmc::TimeSource for DummyClock {
-    fn get_timestamp(&self) -> embedded_sdmmc::Timestamp {
-        embedded_sdmmc::Timestamp {
-            year_since_1970: 0,
-            zero_indexed_month: 0,
-            zero_indexed_day: 0,
-            hours: 0,
-            minutes: 0,
-            seconds: 0,
-        }
+    extern "Rust" {
+        #[task(binds = USART1, shared = [gps], priority = 2)]
+        fn gps_irq(ctx: gps_irq::Context);
+
+        #[task(local = [led], priority = 1)]
+        fn blink(ctx: blink::Context);
+
     }
 }
--- cansat-stm32f4/src/startup.rs
+++ cansat-stm32f4/src/startup.rs
@@ -1,27 +1,66 @@
-use crate::I2c1Devices;
 use crate::{
     error::{Report, WrapErr},
-    Spi2Device,
+    SdLogger,
 };
-use stm32f4xx_hal::{i2c, pac, prelude::*, serial, spi};
+use stm32f4xx_hal::{
+    gpio, i2c, pac,
+    prelude::*,
+    serial, spi,
+    timer::{monotonic::MonoTimerUs, DelayUs},
+};
+
+pub type Monotonic = MonoTimerUs<pac::TIM2>;
+pub type Delay = DelayUs<pac::TIM3>;
+pub type Led = gpio::PC13<gpio::Output>;
+pub type Gps = cansat_gps::Gps<Serial1>;
+pub type SdmmcController =
+    embedded_sdmmc::Controller<BlockSpi2, DummyClock, MAX_OPEN_DIRS, MAX_OPEN_FILES>;
+pub type Bme280 = bme280::i2c::BME280<I2c1Proxy>;
+pub type Lis3dh = lis3dh::Lis3dh<lis3dh::Lis3dhI2C<I2c1Proxy>>;
+
+type BlockSpi2 = embedded_sdmmc::BlockSpi<'static, Spi2, Cs2>;
+const MAX_OPEN_DIRS: usize = 4;
+const MAX_OPEN_FILES: usize = 4;
+
+type I2c1Proxy = shared_bus::I2cProxy<'static, shared_bus::AtomicCheckMutex<I2c1>>;
+type I2c1 = i2c::I2c1<(Scl1, Sda1)>;
+type Scl1 = gpio::PB8<gpio::Alternate<4, gpio::OpenDrain>>;
+type Sda1 = gpio::PB9<gpio::Alternate<4, gpio::OpenDrain>>;
+
+type Serial1 = serial::Serial1<(Tx1, Rx1)>;
+type Tx1 = gpio::PB6<gpio::Alternate<7>>;
+type Rx1 = gpio::PB7<gpio::Alternate<7>>;
+
+pub type Spi2Device = embedded_sdmmc::SdMmcSpi<Spi2, Cs2>;
+type Spi2 = spi::Spi2<(Sck2, Miso2, Mosi2)>;
+type Cs2 = gpio::PB12<gpio::Output>;
+type Sck2 = gpio::PB13<gpio::Alternate<5>>;
+type Miso2 = gpio::PB14<gpio::Alternate<5>>;
+type Mosi2 = gpio::PB15<gpio::Alternate<5>>;
+
 pub struct CanSat {
-    pub monotonic: crate::Monotonic,
-    pub delay: crate::Delay,
-    pub led: crate::Led,
-    pub gps: crate::Gps,
-    pub sd_logger: crate::SdLogger,
+    pub monotonic: Monotonic,
+    pub delay: Delay,
+    pub led: Led,
+    pub gps: Gps,
+    pub sd_logger: SdLogger,
     pub tracker: accelerometer::Tracker,
-    pub i2c1_devices: crate::I2c1Devices,
+    pub i2c1_devices: I2c1Devices,
+}
+
+pub struct I2c1Devices {
+    pub bme280: Bme280,
+    pub lis3dh: Lis3dh,
 }
 
 pub struct Board {
-    pub monotonic: crate::Monotonic,
-    pub delay: crate::Delay,
-    pub led: crate::Led,
-    pub i2c1: crate::I2c1,
-    pub serial1: crate::Serial1,
-    pub spi2: crate::Spi2,
-    pub cs2: crate::Cs2,
+    pub monotonic: Monotonic,
+    pub delay: Delay,
+    pub led: Led,
+    pub i2c1: I2c1,
+    pub serial1: Serial1,
+    pub spi2: Spi2,
+    pub cs2: Cs2,
 }
 
 pub fn init_drivers(
@@ -29,7 +68,7 @@ pub fn init_drivers(
     spi2_device: &'static mut Option<Spi2Device>,
 ) -> Result<CanSat, Report> {
     let i2c1 = board.i2c1;
-    let i2c1_manager = shared_bus::new_atomic_check!(crate::I2c1 = i2c1).unwrap();
+    let i2c1_manager = shared_bus::new_atomic_check!(I2c1 = i2c1).unwrap();
 
     defmt::info!("Initializing sd logger");
     let mut sd_logger = {
@@ -40,16 +79,16 @@ pub fn init_drivers(
                 .unwrap()
                 .acquire()
                 .wrap_err("Failed to acquire block spi")?;
-            crate::SdmmcController::new(block_spi2, crate::DummyClock)
+            SdmmcController::new(block_spi2, DummyClock)
         };
 
-        crate::sd_logger::SdLogger::new(controller).wrap_err("Failed to initialize SdLogger")?
+        SdLogger::new(controller).wrap_err("Failed to initialize SdLogger")?
     };
-    let _ = sd_logger.write(b"[NEW RUN]");
+    let _ = sd_logger.write(b"[NEW RUN]\n");
 
     defmt::info!("Initializing BME280");
     let bme280 = {
-        let mut bme280 = crate::Bme280::new_primary(i2c1_manager.acquire_i2c());
+        let mut bme280 = Bme280::new_primary(i2c1_manager.acquire_i2c());
         bme280
             .init(&mut board.delay)
             .wrap_err("Failed to initialize BME280")?;
@@ -59,12 +98,12 @@ pub fn init_drivers(
     defmt::info!("Initializing GPS");
     let gps = {
         board.serial1.listen(serial::Event::Rxne);
-        crate::Gps::new(board.serial1)
+        Gps::new(board.serial1)
     };
 
     defmt::info!("Initializing LIS3DH");
     let mut lis3dh =
-        crate::Lis3dh::new_i2c(i2c1_manager.acquire_i2c(), lis3dh::SlaveAddr::Default).unwrap();
+        Lis3dh::new_i2c(i2c1_manager.acquire_i2c(), lis3dh::SlaveAddr::Default).unwrap();
     lis3dh.set_range(lis3dh::Range::G8).unwrap();
 
     let tracker = accelerometer::Tracker::new(3700.0);
@@ -142,5 +181,20 @@ pub fn init_board(device: pac::Peripherals) -> Board {
         serial1,
         spi2,
         cs2,
+    }
+}
+
+pub struct DummyClock;
+
+impl embedded_sdmmc::TimeSource for DummyClock {
+    fn get_timestamp(&self) -> embedded_sdmmc::Timestamp {
+        embedded_sdmmc::Timestamp {
+            year_since_1970: 0,
+            zero_indexed_month: 0,
+            zero_indexed_day: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+        }
     }
 }
