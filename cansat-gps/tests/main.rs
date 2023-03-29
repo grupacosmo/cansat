76db359a918593432161e3db693788b39b79c969
Kinga Madej <126254663+kingamadej@users.noreply.github.com>
2023-03-29 18:38:44 +0200

    Finished cansat-gps tests (#48)
    * Finish cansat-gps tests

--- cansat-gps/tests/main.rs
+++ cansat-gps/tests/main.rs
@@ -1,5 +1,6 @@
 use cansat_gps::Gps;
 use cansat_test_utils::mock;
+use rand::Rng;
 
 const FIRST_MSG: &[u8] = b"$GPGLL,,,,,,V,N*64\r\n";
 const SECOND_MSG: &[u8] = b"$GPGLL,,,,,080619.00,V,N*4C\r\n";
@@ -15,29 +16,38 @@ fn gps_last_nmea_returns_none_if_no_msg_received() {
     assert!(gps.last_nmea().is_none());
 }
 
-#[ignore]
 #[test]
 fn gps_last_nmea_returns_msg_if_msg_received() {
     let uart = mock::Serial::new(FIRST_MSG.to_owned());
-    let _ = Gps::new(uart);
+    let mut gps = Gps::new(uart);
 
-    todo!()
+    while let (_, _is_msg_terminated @ false) = gps.read_serial().unwrap() {}
+
+    assert_eq!(gps.last_nmea().unwrap(), FIRST_MSG);
 }
 
-#[ignore]
 #[test]
 fn gps_last_nmea_returns_first_msg_if_second_not_yet_terminated() {
     let uart = mock::Serial::new([FIRST_MSG, SECOND_MSG].concat());
-    let _ = Gps::new(uart);
+    let mut gps = Gps::new(uart);
+    let mut rng = rand::thread_rng();
 
-    todo!()
+    while let (_, _is_msg_terminated @ false) = gps.read_serial().unwrap() {}
+
+    for _ in 0..rng.gen_range(1..SECOND_MSG.len()) {
+        gps.read_serial().unwrap();
+    }
+
+    assert_eq!(gps.last_nmea().unwrap(), FIRST_MSG);
 }
 
-#[ignore]
 #[test]
 fn gps_last_nmea_returns_second_read_msg() {
     let uart = mock::Serial::new([FIRST_MSG, SECOND_MSG].concat());
-    let _ = Gps::new(uart);
+    let mut gps = Gps::new(uart);
 
-    todo!()
+    while let (_, _is_msg_terminated @ false) = gps.read_serial().unwrap() {}
+    while let (_, _is_msg_terminated @ false) = gps.read_serial().unwrap() {}
+
+    assert_eq!(gps.last_nmea().unwrap(), SECOND_MSG);
 }
