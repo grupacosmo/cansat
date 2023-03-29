ecb61ef854521d33d4a14db275ea6e1cfc749e4a
wiktorwieclaw <wiktorwieclaw@users.noreply.github.com>
2023-03-29 16:51:13 +0000

    deploy: 76db359a918593432161e3db693788b39b79c969

--- api/implementors/core/marker/trait.StructuralPartialEq.js
+++ api/implementors/core/marker/trait.StructuralPartialEq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"cansat_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.1/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"struct\" href=\"cansat_core/quantity/struct.Pressure.html\" title=\"struct cansat_core::quantity::Pressure\">Pressure</a>"]]
+"cansat_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.2/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"struct\" href=\"cansat_core/quantity/struct.Pressure.html\" title=\"struct cansat_core::quantity::Pressure\">Pressure</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
